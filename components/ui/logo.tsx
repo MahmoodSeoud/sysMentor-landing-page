73f51aa265e37a89598c1be8397d121c480f2abb
MahmoodSeoud <Mahmoodseoud@gmail.com>
2023-12-25 13:18:00 +0100

    icons

--- components/ui/logo.tsx
+++ components/ui/logo.tsx
@@ -5,7 +5,7 @@ import sysMentorLogo from '@/public/images/sysMentorIcon.png'
 export default function Logo() {
   return (
     <Link href="/" className="block" aria-label="Cruip">
-      <Image src={sysMentorLogo} alt="Cruip" width={32} height={32} />
+      <Image src={sysMentorLogo} alt="Cruip" width={64} height={64} />
     </Link>
   )
 }
